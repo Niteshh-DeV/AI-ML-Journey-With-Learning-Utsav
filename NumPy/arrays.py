b35754cfcc648ef252c4d469c211d90624415c47
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-09-28 19:19:44 +0545

    Different Types of arrays in numerial Python

---
+++ NumPy/arrays.py
@@ -0,0 +1,13 @@
+#NumPy arrays are a powerful data structure for numerical computing in Python. 
+# They provide efficient storage and manipulation of large datasets, and support a wide range of mathematical operations.
+
+import numpy as np
+
+# 1D array
+array1 = np.array([1, 2, 3, 4, 5])
+
+print(array1)
+
+# 2D array
+array2 = np.array([[1, 2, 3], [4, 5, 6]])
+print(array2)
