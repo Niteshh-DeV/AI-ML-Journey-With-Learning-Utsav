37fd47e64d4bef3a434d964d047791ce4b95b36a
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-10-02 21:26:18 +0545

    Stastictics in numpy arrays

--- NumPy/Statistics.py
+++ NumPy/Statistics.py
@@ -24,6 +24,8 @@ print("Min:", min_value)
 print("Max:", max_value)
 
 #argmin and argmax
+# argmin and argmax return the indices of the minimum and maximum values in the array. 
+# For example: The armin of [1, 2, 3, 4, 5] is 0 (the index of the first element, which is 1),
 argmin_index = np.argmin(data)
 argmax_index = np.argmax(data)
 print("Index of Min:", argmin_index)
@@ -36,5 +38,7 @@ print("Sum:", sum_value)
 print("Product:", product_value)
 
 #Cumsum
+# Cumsum returns the cumulative sum of the elements along a given axis. 
+# For example: The cumsum of [1, 2, 3, 4, 5] is [1, 3, 6, 10, 15].
 cumsum_value = np.cumsum(data)
 print("Cumulative Sum:", cumsum_value)
